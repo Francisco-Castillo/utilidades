c081c93ee7caaa77c4afb0e78c59ce026b51dd55
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2020-08-26 16:19:27 -0300

    clase respuesta implementa interface irespuestajson

---
+++ src/main/java/com/fcastillo/utilidades/interfaces/IRespuestaJSON.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fcastillo.utilidades.interfaces;
+
+import java.util.List;
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author fcastillo
+ * @version Agosto 2020
+ */
+public interface IRespuestaJSON {
+
+    /**
+     *
+     * @param statusCode - Codigo de estado HTTP
+     * @param lstMensajes - Lista de mensajes a mostrar al cliente
+     * @param arregloElementos - Listado de elementos a mostrar en consulta, en caso de no retornar listado puede ser null
+     * @param totalRows - Cantidad total de elementos retornados, si no devuelve elementos enviar 0.
+     * @return Respuesta JSON
+     */
+    public Response jsonResponse(int statusCode, List<String> lstMensajes, JsonArrayBuilder arregloElementos, int totalRows);
+
+    /**
+     *
+     * @param statusCode - Representa el codigo de estado a enviar al cliente
+     * @param mensaje - Mensaje a mostrar
+     * @return Respuesta JSON
+     */
+    public Response jsonSimpleResponse(int statusCode, String mensaje);
+
+}
---
+++ src/main/java/com/fcastillo/utilidades/rest/Respuesta.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fcastillo.utilidades.rest;
+
+import com.fcastillo.utilidades.interfaces.IRespuestaJSON;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author fcastillo
+ * @version Agosto 2020
+ */
+public class Respuesta implements IRespuestaJSON {
+
+    @Override
+    public Response jsonResponse(int statusCode, List<String> lstMensajes, JsonArrayBuilder arregloElementos, int totalRows) {
+
+        JsonObjectBuilder job = Json.createObjectBuilder();
+
+        Response.Status status = getStatus(statusCode);
+
+        job.add("status", Json.createObjectBuilder()
+                .add("statusCode", status.getStatusCode())
+                .add("statusMessage", status.getReasonPhrase())
+                .add("listMessages", Json.createArrayBuilder(lstMensajes)))
+                .add("totalRows", totalRows)
+                .add("objeto", arregloElementos).build();
+
+        return Response.status(status).entity(job).build();
+    }
+
+    @Override
+    public Response jsonSimpleResponse(int statusCode, String mensaje) {
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+
+        Response.Status status = getStatus(statusCode);
+
+        job.add("status", Json.createObjectBuilder()
+                .add("statusCode", status.getStatusCode())
+                .add("statusMessage", status.getReasonPhrase()))
+                .add("message", mensaje);
+
+        return Response.status(status).entity(job.build()).build();
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="getStatus()">
+    /**
+     * Método que retorna un objeto de tipo status
+     *
+     * @param statuscode codigo de estado
+     * @return
+     */
+    private Response.Status getStatus(int statuscode) {
+        Response.Status[] arrStatus = Response.Status.values();
+        Response.Status status = Arrays.stream(arrStatus)
+                .filter(x -> x.getStatusCode() == statuscode)
+                .findFirst().orElse(null);
+        return status;
+    }//</editor-fold>
+
+}
